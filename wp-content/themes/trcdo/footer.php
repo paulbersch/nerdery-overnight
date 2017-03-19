9d39bc9eb285f89a22cb4c2ba736f489917c9f15
colt.borg <colt.borg@backstopsolutions.com>
2017-03-19 03:23:54 -0500

    customize-footer - Add footer calls
    Create new footer menu locations for menus to go
    
    Add inital php calls to full menus

--- wp-content/themes/trcdo/footer.php
+++ wp-content/themes/trcdo/footer.php
@@ -14,12 +14,17 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'trcdo' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'trcdo' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'trcdo' ), 'trcdo', '<a href="https://automattic.com/" rel="designer">Chicago Web Friends</a>' ); ?>
-		</div><!-- .site-info -->
+
+			<?php wp_nav_menu( array( 'theme_location' => 'menu-2', 'container_class' => 'footer-nav-container' ) ); ?>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'menu-3', 'container_class' => 'footer-nav-container' ) ); ?>
+
+			<div class="copyright">
+				TRDCO &copy; <?php echo date("Y"); ?>
+			</div>
+	
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
