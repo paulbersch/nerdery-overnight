1290ccf173bb1bdcf2f722eeda1c5e6ad77ccb65
Mica Alaniz <malaniz@leapfrogonline.com>
2017-03-19 01:22:49 -0500

    fixing the pages

---
+++ wp-content/themes/trcdo/page-howtohelp.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template name: How To Help
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<section class="row align-center align-middle">
+				<div class="columns small-11">
+					<h1 class="page-headline">
+					</h1>
+				</div>
+			</section>
+			<section class="row align-center align-middle">
+				<div class="columns small-10">
+					<p class="page-content">
+
+					</p>
+				</div>
+			</section>
+			<section class="row align-center align-middle">
+				<div class="columns small-2">
+					<button class="button primary">
+						Donate
+					</button>
+				</div>
+			</section>
+			<section class="row align-center align-middle">
+				<div class="columns small-10">
+					<p class="page-content">
+
+					</p>
+				</div>
+			</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
