c9c6822cdf780189765397eb2462a5aebaf6188f
Mica Alaniz <malaniz@leapfrogonline.com>
2017-03-19 03:16:07 -0500

    fixing homepage stuff

--- wp-content/themes/trcdo/page-home.php
+++ wp-content/themes/trcdo/page-home.php
@@ -27,7 +27,7 @@ get_header(); ?>
 						<h1>39,857</h1>
 						People Served
 					</div>
-					<div class="columns small-12 large-3 banner-metric">
+					<div class="columns small-12 large-4 banner-metric">
 						<h1>$5.75 Million</h1>
 						In Assistance Received
 					</div>
@@ -40,7 +40,7 @@ get_header(); ?>
 			<section class="programs">
 				<h3 class="program-headline">Programs</h3>
 				<div class="row align-center align-middle">
-					<div class="columns small-12 large-4">
+					<div class="columns small-12 large-3">
 						<div class="program-1">
 							<h2>Housing Assistance<br/></h2>
 							<p>
@@ -48,25 +48,25 @@ get_header(); ?>
 							We offer financial help with mortgage payments, affordable housing coordination, and homeownership cost management.</p>
 						</div>
 					</div>
-					<div class="columns small-12 large-4">
+					<div class="columns small-12 large-3 large-offset-1">
 						<div class="program-2">
 							<h2>Utilities and Energy Assistance</h2>
 							<p>We can help you meet your gas and electric bills, and connect to services to better insulate your home.</p>
 						</div>
 					</div>
-					<div class="columns small-12 large-4">
+					<div class="columns small-12 large-3 large-offset-1">
 						<div class="program-3">
 							<h2>Harvest for the World Food Pantry</h2>
 							<p>Our food pantry feeds hundreds of families and provides worthwhile community service opportunities.</p>
 						</div>
 					</div>
-					<div class="columns small-12 large-4">
+					<div class="columns small-12 large-3">
 						<div class="program-4">
 							<h2>Youth Services</h2>
 							<p>We use a wide variety of programs focused on empowerment and prevention to help at-risk youth between 8 and 21 years of age.</p>
 						</div>
 					</div>
-					<div class="columns small-12 large-4">
+					<div class="columns small-12 large-3 large-offset-1">
 						<div class="program-5">
 						<h2>Health Services</h2>
 						<p>We empower people make realistic changes that lead to a healthy lifestyle. We direct a variety of fun year-round activities.</p>
