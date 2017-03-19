9c1f65d197c3cfe710bfe3480208beb18a48f627
Gabe Masiulis <gabe@masiul.is>
2017-03-18 23:58:09 -0500

    :construction: header navbar and menu

--- wp-content/themes/trcdo/header.php
+++ wp-content/themes/trcdo/header.php
@@ -22,28 +22,18 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'trcdo' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
+		<div class="site-branding top-bar align-center">
+				<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="TRCDO Logo" class="columns">
+				<nav class="text-center show-for-medium columns large-7" data-topbar role="navigation" id="site-navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'menu-1',
+																		'menu_id' => 'primary-menu') ); ?>
+				</nav>
+				<ul class="columns">
+					<li class="show-for-medium">Support</li>
+					<li>Donate</li>
+				</ul>
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'trcdo' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
